4094f8b0f75a2a45dc1c13d21301a71a107b5b8f
CriusNyx <rjreynoldsw@gmail.com>
2026-08-17 12:45:57 -0400

    Added AsOk, AsErr, and Collect

--- Result/src/ResultExtensions.cs
+++ Result/src/ResultExtensions.cs
@@ -21,4 +21,69 @@ public static class ResultExtensions
     }
     return null;
   }
+
+  /// <summary>
+  /// Convert value to an ok result.
+  /// </summary>
+  /// <typeparam name="Value"></typeparam>
+  /// <param name="value"></param>
+  /// <returns></returns>
+  public static Ok<Value> AsOk<Value>(this Value value)
+  {
+    return Result.Ok(value);
+  }
+
+  /// <summary>
+  /// Convert value to an ok result.
+  /// </summary>
+  /// <typeparam name="Value"></typeparam>
+  /// <typeparam name="Error"></typeparam>
+  /// <param name="value"></param>
+  /// <returns></returns>
+  public static Result<Value, Error> AsOk<Value, Error>(this Value value)
+  {
+    return Result.Ok<Value, Error>(value);
+  }
+
+  /// <summary>
+  /// Convert value to an error result.
+  /// </summary>
+  /// <typeparam name="Error"></typeparam>
+  /// <param name="error"></param>
+  /// <returns></returns>
+  public static Err<Error> AsErr<Error>(this Error error)
+  {
+    return Result.Err(error);
+  }
+
+  /// <summary>
+  /// Convert value to an error result.
+  /// </summary>
+  /// <typeparam name="Value"></typeparam>
+  /// <typeparam name="Error"></typeparam>
+  /// <param name="error"></param>
+  /// <returns></returns>
+  public static Result<Value, Error> AsErr<Value, Error>(this Error error)
+  {
+    return Result.Err<Value, Error>(error);
+  }
+
+  /// <summary>
+  /// If any result is an error, return Err&lt;IEnumerable&lt;Error&gt;&gt;.
+  /// If all results are okay then return Ok&lt;IEnumerable&lt;Value&gt;&gt;
+  /// </summary>
+  /// <typeparam name="Value"></typeparam>
+  /// <typeparam name="Error"></typeparam>
+  /// <param name="results"></param>
+  /// <returns></returns>
+  public static Result<IEnumerable<Value>, IEnumerable<Error>> Collect<Value, Error>(
+    this IEnumerable<Result<Value, Error>> results
+  )
+  {
+    if (results.Any(x => x.IsErr()))
+    {
+      return results.Where(x => x.IsErr()).Select(x => x.UnwrapErr()).AsErr();
+    }
+    return results.Select(x => x.Unwrap()).AsOk();
+  }
 }
--- Tests/src/ResultExtensionsTests.cs
+++ Tests/src/ResultExtensionsTests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Frozen;
+using System.Security.Cryptography.X509Certificates;
 using CriusNyx.Results.Extensions;
 
 namespace CriusNyx.Results.Tests;
@@ -16,5 +18,59 @@ public class ResultExtensionsTests
   {
     Result<int, Exception> result = Result.Err(new Exception());
     Assert.That(result.UnwrapOrNull(), Is.Null);
+  }
+
+  [Test]
+  public void AsOk_Works()
+  {
+    Result<string, Exception> result = "Hello".AsOk();
+    Assert.That(result.Unwrap(), Is.EqualTo("Hello"));
+  }
+
+  [Test]
+  public void AsOk_WithErrorType_Works()
+  {
+    Result<string, Exception> result = "Hello".AsOk<string, Exception>();
+    Assert.That(result.Unwrap(), Is.EqualTo("Hello"));
+  }
+
+  [Test]
+  public void AsErr_Works()
+  {
+    Exception exception = new NotImplementedException();
+    Result<string, Exception> result = exception.AsErr();
+    Assert.That(result.UnwrapErr(), Is.EqualTo(exception));
+  }
+
+  [Test]
+  public void AsErr_WithValueType_Works()
+  {
+    Exception exception = new NotImplementedException();
+    Result<string, Exception> result = exception.AsErr<string, Exception>();
+    Assert.That(result.UnwrapErr(), Is.EqualTo(exception));
+  }
+
+  [Test]
+  public void Collect_Ok_Works()
+  {
+    string[] values = ["Hello", "World"];
+    Result<string, Exception>[] results = values.Select(x => x.AsOk<string, Exception>()).ToArray();
+    var result = results.Collect();
+    Assert.That(result.IsOk());
+    Assert.That(result.Unwrap(), Is.EquivalentTo(new string[] { "Hello", "World" }));
+  }
+
+  [Test]
+  public void Collect_Exception_Works()
+  {
+    var exception = new NotImplementedException();
+    Result<string, Exception>[] results =
+    [
+      Result.Ok("Hello"),
+      Result.Err<string, Exception>(exception),
+    ];
+    var result = results.Collect();
+    Assert.That(result.IsErr());
+    Assert.That(result.UnwrapErr(), Is.EquivalentTo(new Exception[] { exception }));
   }
 }
